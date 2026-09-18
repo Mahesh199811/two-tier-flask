a06845b3bc2092d5e33c97b48daaffdaf19bcb07
Mahesh Gadhave <maheshgadhave82@gmail.com>
2026-09-18 10:18:15 +0000

    Test1

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "return "Two-Tier Flask Application v2 - Jenkins CI/CD Working!""
+    return "return "Two-Tier Flask Application v2 - CI/CD Working!""
 
 
 @app.route("/health")
