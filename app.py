110e677ccac40da3de5e7289103e733fcdd91c94
Mahesh Gadhave <maheshgadhave82@gmail.com>
2026-09-18 08:41:47 +0000

    Initial commit: Flask MySQL Docker Compose application

---
+++ app.py
@@ -0,0 +1,54 @@
+from flask import Flask
+import mysql.connector
+import os
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Two-Tier Flask Application is Running!"
+
+
+@app.route("/health")
+def health():
+    return {
+        "status": "healthy"
+    }
+
+
+@app.route("/db")
+def database():
+    try:
+        connection = mysql.connector.connect(
+            host=os.getenv("DB_HOST", "mysql"),
+            user=os.getenv("DB_USER", "appuser"),
+            password=os.getenv("DB_PASSWORD", "apppassword"),
+            database=os.getenv("DB_NAME", "appdb")
+        )
+
+        cursor = connection.cursor()
+
+        cursor.execute("SELECT VERSION()")
+        version = cursor.fetchone()
+
+        cursor.close()
+        connection.close()
+
+        return {
+            "database": "connected",
+            "mysql_version": version[0]
+        }
+
+    except Exception as e:
+        return {
+            "database": "connection failed",
+            "error": str(e)
+        }, 500
+
+
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=5000
+    )
